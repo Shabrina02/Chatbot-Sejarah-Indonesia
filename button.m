0755db6334fc66817aa6e8805d85e934e75133fa
Shabrina02 <77773526+Shabrina02@users.noreply.github.com>
2021-08-08 20:13:11 +0700

    Create button.m

---
+++ button.m
@@ -0,0 +1,25 @@
+\
+\ Copyright © 2021 shabrina all rights reserved.
+\
+\ @author: shabrina
+\ @date: 8 Aug 2021
+\
+\ A brief description of this program.
+\
+
+: button ( "s" -- "s" )
+  q{
+      background-color: #708090;
+      border: none;
+      width: 75%;
+      color: white;
+      padding: 10px 24px;
+      text-align: center;
+      text-decoration: none;
+      display: block;
+      font-size: 14px;
+      font-family: Helvetica;
+  }q (button)
+;
+
+
