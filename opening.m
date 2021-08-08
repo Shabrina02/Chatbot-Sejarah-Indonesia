12f17556021ed4246946ac247b9af2252e26d9fd
Shabrina02 <77773526+Shabrina02@users.noreply.github.com>
2021-08-08 20:13:35 +0700

    Create opening.m

---
+++ opening.m
@@ -0,0 +1,22 @@
+\
+\ Copyright © 2021 shabrina all rights reserved.
+\
+\ @author: shabrina
+\ @date: 7 Aug 2021
+\
+\ A brief description of this program.
+\
+
+: opening
+  q{
+    <p align="center"><b>WELCOME CHASWAN!</b>
+    <img src="https://i.ibb.co/stXk8v2/logo-11.gif" width="200" height="200">
+    <p align="justify">Halo! Aku adalah<b>CHASI</b>, chatbot sejarah Indonesia.<br>
+    Aku akan membawamu kembali ke peristiwa-peristiwa penting 
+    sebelum kemerdekaan Indonesia.<br>
+    Dibeberapa peristiwa tersebut, kamu bisa memilih peran sendiri, lho 😉.
+    </p>
+  }q
+;
+
+
